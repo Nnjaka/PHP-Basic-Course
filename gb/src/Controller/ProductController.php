fe35a88e62cf0e180879ea33461d43bb59fec5a0
Irina Vinter <irishka2150@rambler.ru>
2021-12-02 23:41:07 +0700

    lesson-8

---
+++ gb/src/Controller/ProductController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product", name="product")
+     */
+    public function index(): Response
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $repository = $manager->getRepository(Product::class);
+        $products = $repository->findAll();
+       
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+     /**
+     * @Route("/product/create", name="create_product")
+     */
+    public function createProduct(): Response
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $product = new Product();
+        $product->setTitle('Title ' . random_int(0, 100));
+        $product->setPrice(random_int(1, 1000));
+        $product->setDescription('');
+        $manager->persist($product);
+        $manager->flush();
+
+        return $this->redirectToRoute('product');
+
+    }
+}
